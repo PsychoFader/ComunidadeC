c46e20e806e290c9c2aba09a35eec592d0b0103e
ScorpioW <31605252+ScorpioW@users.noreply.github.com>
2018-04-17 15:42:28 +0100

    FESTA EM LOUCURA
    MLG MY FRIENDSSS SQRRR POP POP

---
+++ index.php
@@ -0,0 +1,123 @@
+<?php include('header.php'); ?>
+		<!-- Main -->
+			<div class="wrapper style2">
+				<section class="container">
+					<header class="major">
+						<h2>2017 FIA Fórmula 1 Campeonato do Mundo ® Calendário de Corridas</h2>
+					</header>
+					<div class="container">
+					  <div class="row">
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+					      <a href="corridas_australia.php" class="zoom"><img src="images/ico-aust-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>24 - 26 MAR</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA AUSTRÁLIA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+					      <a href="corridas_china.php" class="zoom"><img src="images/ico-chin-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>07 - 09 ABR</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA CHINA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+					     <a href="corridas_barem.php" class="zoom"><img src="images/ico-bahrain-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>14 - 16 ABR</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DO BARÉM</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+				      		<a href="corridas_russia.php" class="zoom"><img src="images/ico-rus-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>28 - 30 ABR</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA RUSSIA</h4>
+				    	</div>
+				    	<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+				      		<a href="corridas_espanha.php" class="zoom"><img src="images/ico-esp-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>12 - 14 MAI</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DE ESPANHA</h4>
+				    	</div>
+				    	<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+				      		<a href="corridas_monaco.php" class="zoom"><img src="images/ico-mon-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>25 - 28 MAI</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DO MÓNACO</h4>
+				    	</div>
+				    	<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+					     	<a href="corridas_canada.php" class="zoom"><img src="images/ico-can-gp.jpeg" width="293" height="143" alt=""></a>
+							<h5>09 - 11 JUN</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DO CANADÁ</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+					    	<a href="corridas_azerbeijao.php" class="zoom"><img src="images/ico-azer-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>23 - 25 JUN</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DE AZERBAIJÃO</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_austria.php" class="zoom"><img src="images/ico-austria-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>24 - 26 NOV</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA AUSTRIA</h4>
+					    </div>					    
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_inglaterra.php" class="zoom"><img src="images/ico-eng-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>14 - 16 JUL</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA INGLATERRA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_hungria.php" class="zoom"><img src="images/ico-hun-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>28 - 30 JUL</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA HUNGRIA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_belgica.php" class="zoom"><img src="images/ico-belg-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>25 -27 AGO</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA BÉLGICA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_italia.php" class="zoom"><img src="images/ico-ita-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>01 - 03 SET</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA ITÁLIA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_singapura.php" class="zoom"><img src="images/ico-sing-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>14 - 17 SET</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA SINGAPURA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_malasia.php" class="zoom"><img src="images/ico-mal-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>29 SET - 01 OUT</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DA MALÁSIA</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_japao.php" class="zoom"><img src="images/ico-jap-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>06 - 08 OUT</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DO JAPÃO</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_usa.php" class="zoom"><img src="images/ico-us-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>20 - 22 OUT</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DOS ESTADOS UNIDOS</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+					      	<a href="corridas_mexico.php" class="zoom"><img src="images/ico-mex-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>27 - 29 OUT</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DO MÉXICO</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_brasil.php" class="zoom"><img src="images/ico-bra-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>10 - 12 NOV</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DO BRASIL</h4>
+					    </div>
+					    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12  filtr-item">
+							<a href="corridas_abu.php" class="zoom"><img src="images/ico-abu-gp.jpg" width="293" height="143" alt=""></a>
+							<h5>24 - 26 NOV</h5>
+							<h4>2017 FORMULA 1 GRANDE PRÉMIO DE ABU DHABI</h4>
+					    </div>
+
+					  </div>
+					</div>
+
+				<div class="container">
+				  <div class="row">				    
+				  </div>
+				</div>
+					<div class="row no-collapse-1">
+					</div>
+				</section>
+			</div>
+
+		<?php include('footer.php'); ?>
