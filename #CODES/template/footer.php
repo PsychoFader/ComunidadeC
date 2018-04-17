f8529adc76ada26c4032aa30a8605a8fa88927db
anamb06 <ana-maria2000@live.com.pt>
2018-04-17 15:43:55 +0100

    update

--- #CODES/template/footer.php
+++ #CODES/template/footer.php
@@ -1,66 +1,64 @@
-<!-- Footer -->
-			<div id="footer">
+			<div id="footer" style="background-color: #222833;">
 				<div class="container">
 
 					<!-- Lists -->
-					<div class="row">
-						<div class="8u">
-							<section>
-								<header class="major">
-									<h2>Donec dictum metus</h2>
-									<span class="byline">Quisque semper augue mattis wisi maecenas ligula</span>
-								</header>
-								<div class="row">
-									<section class="6u">
-										<ul class="default">
-											<li><a href="#">Pellentesque elit non gravida blandit.</a></li>
-											<li><a href="#">Lorem ipsum dolor consectetuer elit.</a></li>
-											<li><a href="#">Phasellus nibh pellentesque congue.</a></li>
-											<li><a href="#">Cras vitae metus aliquam  pharetra.</a></li>
-										</ul>
-									</section>
-									<section class="6u">
-										<ul class="default">
-											<li><a href="#">Pellentesque elit non gravida blandit.</a></li>
-											<li><a href="#">Lorem ipsum dolor consectetuer elit.</a></li>
-											<li><a href="#">Phasellus nibh pellentesque congue.</a></li>
-											<li><a href="#">Cras vitae metus aliquam  pharetra.</a></li>
-										</ul>
-									</section>
-								</div>
-							</section>
+						<div class="row">
+							<div class="8u">
+								<section>
+									<header class="major">
+										<h2>Créditos</h2>
+									</header>
+									<div class="row">
+										<section class="6u">
+											<ul class="default">
+												<li>Ana Boriceanu Nº1</li>
+												<li>André Costa Nº2</li>
+												<li>Dinis Correia Nº3</li>
+												<li>Diogo Torres Nº5</li>
+											</ul>
+										</section>
+										<section class="6u">
+											<ul class="default">
+												<li>Gabriel Brandão Nº6</li>
+												<li>Gustavo Souza Nº8</a></li>
+												<li>Jean Silva Nº9</li>
+												<li>João Miguel Nº10</li>
+											</ul>
+										</section>
+									</div>
+								</section>
+							</div>
+							<div class="4u">
+								<section>
+									<header class="major">
+										<h2>Equipa de Desenvolvimento</h2>
+										<span class="byline">12ºC Escola Secundária Manuel Teixeira Gomes</span>
+									</header>
+									<ul class="contact">
+										<li>
+											<span class="address">Product Owner</span>
+											<span>Daniel Silva</span>
+										</li>
+										<li>
+											<span class="mail">Scrum Master</span>
+											<span>Gonçalo Camacho</span>
+										</li>
+										<li>
+											<span class="phone">Professor</span>
+											<span>António Meireles</span>
+										</li>
+									</ul>	
+								</section>
+							</div>
 						</div>
-						<div class="4u">
-							<section>
-								<header class="major">
-									<h2>Donec dictum metus</h2>
-									<span class="byline">Mattis wisi maecenas ligula</span>
-								</header>
-								<ul class="contact">
-									<li>
-										<span class="address">Address</span>
-										<span>1234 Somewhere Road #4285 <br />Nashville, TN 00000</span>
-									</li>
-									<li>
-										<span class="mail">Mail</span>
-										<span><a href="#">someone@untitled.tld</a></span>
-									</li>
-									<li>
-										<span class="phone">Phone</span>
-										<span>(000) 000-0000</span>
-									</li>
-								</ul>	
-							</section>
-						</div>
-					</div>
 
 					<!-- Copyright -->
-					<div class="copyright">
-						Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
-					</div>
+						<div class="copyright">
+							Design: <a href="http://templated.co">TEMPLATED</a>
+						</div>
 
 				</div>
 			</div>
 
-		</body>
-		</html>
+	</body>
+</html>
